21eaeee84e7fc6a6d317917695b387e3a3b23203
aleksandr-agadzhanov <al.agadzhanov@gmail.com>
2026-08-14 20:55:51 +0100

    Fix token counts and update vocabulary file for consistency

--- train.py
+++ train.py
@@ -2,15 +2,11 @@ from tokenizers.regex_tokenizer import RegexTokenizer
 
 INPUT_FILE_NAME = "fineweb_edu_100mb.txt"
 NUM_BASE_TOKENS = 256
-NUM_MERGED_TOKENS = 16384 - 256
+NUM_MERGED_TOKENS = 16383 - 256
 SPECIAL_TOKENS = {
-    "<|endoftext|>": 16385,
-    "<|fim_prefix|>": 16386,
-    "<|fim_middle|>": 16387,
-    "<|fim_suffix|>": 16388,
-    "<|endofprompt|>": 16389,
+    "<|endoftext|>": 16383
 }
-VOCABULARY_FILE_NAME = "fineweb_edu_100mb_16389.pkl"
+VOCABULARY_FILE_NAME = "fineweb_edu_100mb_16384.pkl"
 
 vocabulary_size = NUM_BASE_TOKENS + NUM_MERGED_TOKENS + len(SPECIAL_TOKENS)
 
